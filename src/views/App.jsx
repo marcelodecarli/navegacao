e9b7126f0afbdcfcb19219147491ad487e9e2542
Marcelo <sl_marcelo@hotmail.com>
2024-04-22 18:25:52 -0300

    update project functional

---
+++ src/views/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from "react";
+import { BrowserRouter } from 'react-router-dom'
+
+import Content from '../components/layout/Content';
+import Menu from '../components/layout/Menu';
+
+
+const App = props => (
+    <div className="App">
+        <BrowserRouter>
+            <Menu />
+            <Content />
+        </BrowserRouter>
+    </div>
+)
+
+export default App
